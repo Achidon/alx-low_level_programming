bc8556670a54b6e103d125c167a3b28c71795973
Achilonu Joseph <jachilonu2@gmail.com>
2023-06-20 16:08:27 -0700

    message

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,13 @@
 #include "main.h"
 /**
- * main - Entry point
- *
- * print_alphabet - Make the alphabe
+ * print_alphabet - Prints the alphabet to lowercase
  *
  * Return: Always 0 (success)
  */
 void print_alphabet(void)
 {
 	char c;
-
+/* print_alphabet */
 	for (c = 'a'; c <= 'z'; c++)
 	{
 		_putchar(c);
