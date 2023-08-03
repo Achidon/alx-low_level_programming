2459b84facf56ce95bf8e828f7a1ec1906b77ca0
Achilonu Joseph <jachilonu2@gmail.com>
2023-08-03 12:48:23 -0700

    message

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -5,21 +5,20 @@
  *
  * Return: decimal (unsigned int)
  */
-
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int decimal;
-	unsigned int i;
+	int i;
+	unsigned int dec_val = 0;
 
-	for (decimal = 0, i = 0; b[i] != '\0'; i++)
+	if (!b)
+		return (0);
+
+	for (i = 0; b[i]; i++)
 	{
-		if (b[i] == '1')
-			decimal = (decimal << 1) | 1;
-		else if (b[i] == '0')
-			decimal <<= 1;
-		else if (b[i] != '0' && b[i] != '1')
+		if (b[i] < '0' || b[i] > '1')
 			return (0);
+		dec_val = 2 * dec_val + (b[i] - '0');
 	}
 
-	return (decimal);
+	return (dec_val);
 }
